594d5a774b902827275290320963afdb191a6755
Faiz2126 <faizal.al2126@gmail.com>
2024-07-31 09:56:52 +0700

    new website

--- js/script.js
+++ js/script.js
@@ -3,3 +3,20 @@ document.addEventListener("DOMContentLoaded", function() {
     let clone = isiGulir.cloneNode(true);
     isiGulir.parentElement.appendChild(clone);
 });
+
+function smoothScroll() {
+    var anchorLinks = document.querySelectorAll('a[href^="#"]');
+
+    anchorLinks.forEach(function(link) {
+        link.addEventListener('click', function(event) {
+            event.preventDefault();
+            var targetId = this.getAttribute('href');
+            var targetElement = document.querySelector(targetId);
+
+            window.scroll({
+                top: targetElement.offsetTop,
+                behavior: 'smooth'
+            });
+        });
+    });
+}
